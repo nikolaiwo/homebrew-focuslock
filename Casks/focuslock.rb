94d85a3c2c9c5b1d9ca4f6bcdbc96b45d2f5a3f9
Nikolai Orheim <nikolai.orheim@volue.com>
2026-01-21 14:26:22 +0100

    chore: update FocusLock to v1.0.0 with real SHA256

--- Casks/focuslock.rb
+++ Casks/focuslock.rb
@@ -1,6 +1,6 @@
 cask "focuslock" do
   version "1.0.0"
-  sha256 "PLACEHOLDER"
+  sha256 "29055d5785bad48a36f850c770a5c0efda07a3aba74b08755a5a49f0e11cb379"
 
   url "https://github.com/nikolaiwo/focus-lock/releases/download/v#{version}/FocusLock.zip"
   name "FocusLock"
